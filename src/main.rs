fe0eab81e363ebf016a266f4a4f60d64852b849e
robmikh <rob.mikh@outlook.com>
2021-11-17 19:11:44 -0800

    open dwm process handle

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 mod handle;
+mod privilege;
 mod process;
+mod wide_string;
 
 use windows::{
-    core::Result,
-    Win32::System::{RemoteDesktop::ProcessIdToSessionId, Threading::GetCurrentProcessId},
+    core::{Handle, Result},
+    Win32::System::{
+        RemoteDesktop::ProcessIdToSessionId,
+        Threading::{GetCurrentProcessId, OpenProcess, PROCESS_QUERY_INFORMATION, PROCESS_VM_READ},
+    },
 };
 
-use crate::{handle::AutoCloseHandle, process::ProcessIterator};
+use crate::{privilege::set_debug_privilege, process::ProcessIterator};
 
 fn main() -> Result<()> {
+    // We first need to give ourselves debug privileges.
+    set_debug_privilege(true)?;
+
+    // During RDP sessions, you'll have multiple sessions and muiltple
+    // DWMs. We want the one the user is currently using, so find the
+    // session our program is running in.
     println!("Getting the current session...");
     let current_session = unsafe {
         let current_pid = GetCurrentProcessId();
@@ -18,10 +29,23 @@ fn main() -> Result<()> {
     };
     println!("Current session id: {}", current_session);
 
+    // Find the dwm for the session
     println!("Looking for the dwm process of the current session...");
     let process_id = find_dwm_process_id_in_session(current_session)?
         .expect("Could not find a dwm process for this session!");
     println!("Found dwm.exe with pid: {}", process_id);
+
+    // This might fail if we aren't running as admin
+    let process_handle = unsafe {
+        OpenProcess(
+            PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
+            false,
+            process_id,
+        )
+        .ok()?
+    };
+
+    println!("Taking memory dump...");
 
     Ok(())
 }
---
+++ src/privilege.rs
@@ -0,0 +1,71 @@
+use windows::{
+    core::{Handle, Result},
+    Win32::{
+        Foundation::{HANDLE, LUID, PWSTR},
+        Security::{
+            AdjustTokenPrivileges, LookupPrivilegeValueW, LUID_AND_ATTRIBUTES,
+            SE_PRIVILEGE_ENABLED, TOKEN_ADJUST_PRIVILEGES, TOKEN_PRIVILEGES,
+            TOKEN_PRIVILEGES_ATTRIBUTES,
+        },
+        System::Threading::{GetCurrentProcess, OpenProcessToken},
+    },
+};
+
+use crate::{
+    handle::AutoCloseHandle,
+    wide_string::{ToWide, WideString},
+};
+
+pub fn set_debug_privilege(enable: bool) -> Result<()> {
+    let token = get_process_token()?;
+    let se_debug_name = get_debug_privilege_name();
+    set_privilege(&token.0, se_debug_name.as_pwstr(), enable)?;
+    Ok(())
+}
+
+fn get_process_token() -> Result<AutoCloseHandle> {
+    unsafe {
+        // This is a pseudo-handle, so we don't need to close or check it.
+        let process_handle = GetCurrentProcess();
+        let mut result = HANDLE(0);
+        OpenProcessToken(process_handle, TOKEN_ADJUST_PRIVILEGES, &mut result).ok()?;
+        Ok(AutoCloseHandle(result))
+    }
+}
+
+fn set_privilege(token: &HANDLE, privilege_name: PWSTR, enable: bool) -> Result<()> {
+    let mut luid = LUID::default();
+    let attribute = if enable {
+        SE_PRIVILEGE_ENABLED
+    } else {
+        TOKEN_PRIVILEGES_ATTRIBUTES(0)
+    };
+    unsafe {
+        LookupPrivilegeValueW(None, privilege_name, &mut luid).ok()?;
+        let token_privileges = TOKEN_PRIVILEGES {
+            PrivilegeCount: 1,
+            Privileges: [LUID_AND_ATTRIBUTES {
+                Luid: luid,
+                Attributes: attribute,
+            }],
+            ..Default::default()
+        };
+        AdjustTokenPrivileges(
+            token,
+            false,
+            &token_privileges,
+            0,
+            std::ptr::null_mut(),
+            std::ptr::null_mut(),
+        )
+        .ok()?;
+    }
+    Ok(())
+}
+
+// Workaround for missing SE_DEBUG_NAME
+// https://docs.microsoft.com/en-us/windows/win32/secauthz/privilege-constants
+fn get_debug_privilege_name() -> WideString {
+    let string = "SeDebugPrivilege".to_wide();
+    string
+}
