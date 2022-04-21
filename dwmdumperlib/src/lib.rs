f5d9f27b3589c8b947748871448e85ba10a078e0
robmikh <rob.mikh@outlook.com>
2022-04-21 13:10:04 -0700

    refactor lib

---
+++ dwmdumperlib/src/lib.rs
@@ -0,0 +1,57 @@
+mod debug;
+mod handle;
+mod integrity;
+mod privilege;
+mod process;
+
+pub use integrity::IntegrityLevel;
+pub use privilege::set_debug_privilege;
+
+use integrity::{get_current_process_token, get_integrity_level_from_process_token};
+use process::ProcessIterator;
+use windows::{core::Result, Win32::System::{Threading::GetCurrentProcessId, RemoteDesktop::ProcessIdToSessionId}};
+
+use crate::debug::take_memory_dump;
+
+pub fn get_session_for_current_process() -> Result<u32> {
+    unsafe {
+        let current_pid = GetCurrentProcessId();
+        let mut session = 0;
+        ProcessIdToSessionId(current_pid, &mut session).ok()?;
+        Ok(session)
+    }
+}
+
+pub fn find_and_dump_dwm(session_id: u32, file_name: &str) -> Result<()> {
+    // Find the dwm for the session
+    println!("Looking for the dwm process of the current session...");
+    let process_id = find_dwm_process_id_in_session(session_id)?
+        .expect("Could not find a dwm process for this session!");
+    println!("Found dwm.exe with pid: {}", process_id);
+
+    // Take the memory dump
+    println!("Taking memory dump...");
+    take_memory_dump(process_id, file_name)?;
+
+    Ok(())
+}
+
+pub fn find_dwm_process_id_in_session(session_id: u32) -> Result<Option<u32>> {
+    let mut result = None;
+    let process_iter = ProcessIterator::new()?;
+    for process_info in process_iter {
+        if process_info.name().starts_with("dwm.exe") {
+            if process_info.session_id() == session_id {
+                let process_id = process_info.process_id();
+                result = Some(process_id);
+                break;
+            }
+        }
+    }
+    Ok(result)
+}
+
+pub fn get_integrity_level_for_current_process() -> Result<IntegrityLevel> {
+    let process_token = get_current_process_token();
+    get_integrity_level_from_process_token(&process_token)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-mod debug;
-mod handle;
 mod hotkey;
-mod integrity;
-mod privilege;
-mod process;
 
+use dwmdumperlib::{get_integrity_level_for_current_process, IntegrityLevel, set_debug_privilege, find_and_dump_dwm, get_session_for_current_process};
 use hotkey::HotKey;
 use windows::{
     core::Result,
     Win32::{
         Foundation::HWND,
-        System::{RemoteDesktop::ProcessIdToSessionId, Threading::GetCurrentProcessId},
         UI::{
             Input::KeyboardAndMouse::{MOD_CONTROL, MOD_SHIFT},
             WindowsAndMessaging::{DispatchMessageW, GetMessageW, MSG, WM_HOTKEY},
@@ -18,23 +13,13 @@ use windows::{
     },
 };
 
-use crate::{
-    debug::take_memory_dump,
-    integrity::{
-        get_current_process_token, get_integrity_level_from_process_token, IntegrityLevel,
-    },
-    privilege::set_debug_privilege,
-    process::ProcessIterator,
-};
-
 fn main() -> Result<()> {
     // TODO: Configurable file name/path
     let file_name = "dwmdump.dmp";
 
     // Make sure we're running as admin
-    let process_token = get_current_process_token();
     let integrity_level =
-        get_integrity_level_from_process_token(&process_token).unwrap_or(IntegrityLevel::Untrusted);
+        get_integrity_level_for_current_process().unwrap_or(IntegrityLevel::Untrusted);
     println!(
         "Currently detected integrity level: {}",
         integrity_level.display_str()
@@ -52,12 +37,7 @@ fn main() -> Result<()> {
     // DWMs. We want the one the user is currently using, so find the
     // session our program is running in.
     println!("Getting the current session...");
-    let current_session = unsafe {
-        let current_pid = GetCurrentProcessId();
-        let mut session = 0;
-        ProcessIdToSessionId(current_pid, &mut session).ok()?;
-        session
-    };
+    let current_session = get_session_for_current_process()?;
     println!("Current session id: {}", current_session);
 
     // Wait for the user to press the key-combo before we
@@ -74,35 +54,6 @@ fn main() -> Result<()> {
 
     println!("Done! Dump written to \"{}\"", dump_path.display());
     Ok(())
-}
-
-fn find_and_dump_dwm(session_id: u32, file_name: &str) -> Result<()> {
-    // Find the dwm for the session
-    println!("Looking for the dwm process of the current session...");
-    let process_id = find_dwm_process_id_in_session(session_id)?
-        .expect("Could not find a dwm process for this session!");
-    println!("Found dwm.exe with pid: {}", process_id);
-
-    // Take the memory dump
-    println!("Taking memory dump...");
-    take_memory_dump(process_id, file_name)?;
-
-    Ok(())
-}
-
-fn find_dwm_process_id_in_session(session_id: u32) -> Result<Option<u32>> {
-    let mut result = None;
-    let process_iter = ProcessIterator::new()?;
-    for process_info in process_iter {
-        if process_info.name().starts_with("dwm.exe") {
-            if process_info.session_id() == session_id {
-                let process_id = process_info.process_id();
-                result = Some(process_id);
-                break;
-            }
-        }
-    }
-    Ok(result)
 }
 
 fn pump_messages<F: FnMut() -> Result<bool>>(mut hot_key_callback: F) -> Result<()> {
